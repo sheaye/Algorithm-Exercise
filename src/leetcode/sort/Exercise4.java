160638b088e227359637f1afb7c4a27bbafe7983
Snail <1134043635@qq.com>
2018-03-19 13:39:26 +0800

    commit

---
+++ src/leetcode/sort/Exercise4.java
@@ -0,0 +1,57 @@
+package leetcode.sort;
+
+import common.ListNode;
+
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ */
+public class Exercise4 {
+
+    public static class Solution {
+
+        public ListNode sortList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode mid = findMiddle(head);
+            ListNode right = sortList(mid.next);
+            mid.next = null;
+            ListNode left = sortList(head);
+            return merge(left, right);
+        }
+
+        public ListNode findMiddle(ListNode list) {
+            ListNode slow = list;
+            ListNode fast = list.next;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+
+        private ListNode merge(ListNode left, ListNode right) {
+            ListNode list = new ListNode(0);
+            ListNode head = list;
+            while (left != null && right != null) {
+                if (left.val > right.val) {
+                    head.next = right;
+                    right = right.next;
+                } else {
+                    head.next = left;
+                    left = left.next;
+                }
+                head = head.next;
+            }
+            if (left != null) {
+                head.next = left;
+            }
+            if (right != null) {
+                head.next = right;
+            }
+            return list.next;
+        }
+    }
+
+}
---
+++ src/leetcode/sort/Exercise5.java
@@ -0,0 +1,80 @@
+package leetcode.sort;
+
+import common.ListNode;
+
+/**
+ * Sort a linked list using insertion sort.
+ */
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        Exercise5 exe = new Exercise5();
+        int[] array = {1, 3, 5, 2, 4, 6};
+        System.out.println(exe.insertionSortList2(ListNode.create(array)));
+    }
+
+    public ListNode insertionSortList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp;
+        dummyHead.next = insertionSortList2(head.next);
+        temp = dummyHead;
+        while (temp != null && temp.next != null && head.val > temp.next.val) {
+            temp = temp.next;
+        }
+        head.next = temp.next;
+        temp.next = head;
+        return dummyHead.next;
+    }
+
+    public ListNode insertionSortList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode sorted = new ListNode(Integer.MIN_VALUE);
+        ListNode temp;
+        while (head != null) {
+            temp = head.next;
+            head.next = null;
+            sorted = insertionSort(sorted, head);
+            head = temp;
+        }
+
+        return sorted.next;
+    }
+
+    private ListNode insertionSort(ListNode sorted, ListNode next) {
+        if (sorted.next == null) {
+            if (next.val > sorted.val) {
+                sorted.next = next;
+                return sorted;
+            } else {
+                next.next = sorted;
+                return next;
+            }
+        }
+        ListNode mid = findMiddle(sorted);
+        if (next.val > mid.val) {
+            mid.next = insertionSort(mid.next, next);
+            return sorted;
+        } else {
+            ListNode temp = mid.next;
+            mid.next = null;
+            ListNode head = insertionSort(sorted, next);
+            mid.next = temp;
+            return head;
+        }
+    }
+
+    private ListNode findMiddle(ListNode start) {
+        ListNode slow = start;
+        ListNode fast = start.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
